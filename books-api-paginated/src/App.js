4d414a892359566815c3784988e0af429dbcec05
Patrick Harris <patricknpharris@gmail.com>
2020-08-09 16:47:57 +0100

    Add tested axios call to get list of books with temporary react bootstrap card

--- books-api-paginated/src/App.js
+++ books-api-paginated/src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import getBooks from "./getBooks";
+import "./styles/App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [books, setBooks] = useState();
+
+    async function search() {
+        const results = await getBooks();
+        setBooks(results.books);
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Button onClick={search}>Button</Button>
+                {books ? (
+                    <Card>
+                        <Card.Body>{books[0].book_title}</Card.Body>
+                    </Card>
+                ) : (
+                    <></>
+                )}
+            </header>
+        </div>
+    );
 }
 
 export default App;
---
+++ books-api-paginated/src/getBooks.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+
+const api = "http://nyx.vima.ekt.gr:3000/api/books";
+
+async function getBooks() {
+    const searchParams = {page: 1}
+    return await axios.post(api, searchParams).then(response => response.data);
+}
+
+export default getBooks;
